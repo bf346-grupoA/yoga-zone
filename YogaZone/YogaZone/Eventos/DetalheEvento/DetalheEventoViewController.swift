88d6148cf82358c6bfb500c538d3cc59d3d7caeb
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-02-23 20:49:16 -0300

    Incluindo tela detalhe do evento

---
+++ YogaZone/YogaZone/Eventos/DetalheEvento/DetalheEventoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetalheEventoViewController.swift
+//  YogaZone
+//
+//  Created by Rafael Benjamin on 23/02/22.
+//
+
+import UIKit
+
+class DetalheEventoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
