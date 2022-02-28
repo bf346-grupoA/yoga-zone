df1f5579eef92703ab85bcfc98ff6917f871baaf
marcusungarette <marcusungarette@gmail.com>
2022-02-28 15:33:10 -0300

    add imagem treino finalizado

---
+++ YogaZone/YogaZone/Exercicios/TreinoFinalizado/TreinoFinalizadoVC.swift
@@ -0,0 +1,18 @@
+//
+//  TreinoFinalizadoVC.swift
+//  YogaZone
+//
+//  Created by Marcus on 28/02/22.
+//
+
+import UIKit
+
+class TreinoFinalizadoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
