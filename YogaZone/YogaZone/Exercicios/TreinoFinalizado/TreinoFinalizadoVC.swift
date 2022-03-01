f70c50c8f1e86524ca4f9c0e95026ad1eab208f6
marcusungarette <marcusungarette@gmail.com>
2022-02-28 23:28:46 -0300

    Tela Treino Finalizado montada

--- YogaZone/YogaZone/Exercicios/TreinoFinalizado/TreinoFinalizadoVC.swift
+++ YogaZone/YogaZone/Exercicios/TreinoFinalizado/TreinoFinalizadoVC.swift
@@ -9,10 +9,30 @@ import UIKit
 
 class TreinoFinalizadoVC: UIViewController {
 
+    @IBOutlet weak var parabensTrofeuImage: UIImageView!
+    
+    @IBOutlet weak var CongratulationsLabel: UILabel!
+    
+    @IBOutlet weak var CongratulationsDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var CongratulationsCaloriesLabel: UILabel!
+    
+    @IBOutlet weak var SocialMediaShareButton: UIButton!
+    
+    @IBOutlet weak var GoToHomeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        parabensTrofeuImage.image = UIImage(named: "parabensTrofeuImage")
+        
     }
 
+    @IBAction func tappedSocialMediaButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func tappedGoToHomeButton(_ sender: UIButton) {
+    }
+    
+    
+    
 }
--- YogaZone/YogaZone/SceneDelegate.swift
+++ YogaZone/YogaZone/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let navController = UINavigationController(rootViewController: TreinoUmController())
+        let navController = UINavigationController(rootViewController: TreinoFinalizadoVC())
         window = UIWindow(windowScene: windowScene)
         window?.frame = UIScreen.main.bounds
         window?.rootViewController = navController
