ff69879440d36a37f7901ca122268f3b0eaee54c
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-03-06 15:06:01 -0300

    Ajustando ligacoes do detalhe do evento

--- YogaZone/YogaZone/Events/EventDetail/EventDetailViewController.swift
+++ YogaZone/YogaZone/Events/EventDetail/EventDetailViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class EventDetailViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var eventDateLabel: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var eventHoutLabel: UILabel!
+    @IBOutlet weak var numberOfParticipantsLabel: UILabel!
+    @IBOutlet weak var eventNumberOfParticipantsLabel: UILabel!
+    @IBOutlet weak var localLabel: UILabel!
+    @IBOutlet weak var eventLocalLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var availableVacanciesLabel: UILabel!
+    @IBOutlet weak var eventAvailableVacanciesLabel: UILabel!
+    @IBOutlet weak var confirmParticipationButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: UI Functions
+extension EventDetailViewController {
+    func setupUI(){
+        titleLabel.text = "Evento"
+        nameLabel.text = "Nome"
+        dateLabel.text = "Data"
+        hourLabel.text = "Hora"
+        numberOfParticipantsLabel.text = "Participantes"
+        localLabel.text = "Local"
+        descriptionLabel.text = "Descrição"
+        availableVacanciesLabel.text = "Vagas disponíveis"
+        confirmParticipationButton.setTitle("Confirmar Participação", for: .normal)
+        
     }
-    */
-
+    
 }
