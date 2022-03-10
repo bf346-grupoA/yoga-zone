f9471316ff37d346721d24bb580023648986935d
Marcelo Silva <marcelos89@icloud.com>
2022-03-09 21:33:51 -0300

    models e enums padroes criados , alteracoes de label telaResultadoController

--- YogaZone/YogaZone/IMC/Models/Result.swift
+++ YogaZone/YogaZone/IMC/Models/Result.swift
@@ -10,6 +10,30 @@ enum HealthStatus:String{
     case saudavel = "Saudavel"
     case sobrepeso = "SobrePeso"
     case obesidade = "Obesidade"
+    
+    
+enum possibleConsequences:String{
+    case magreza = "Estresse, ansiedade,fadiga"
+    case saudavel = "Menor risco para doenças"
+    case sobrepeso = "Fadiga, varizes, má circulação"
+    case obesidade = "Diabetes, infarto, AVC"
+    
+enum imcNumber: String{
+    case magreza = "<18.5"
+    case saudavel = "18.5 a 25.9"
+    case sobrepeso = "25 a 29.9"
+    case Obesidade = ">29.9"
+        
+
+ 
+        
+    }
+    
+    
+   
+
+
+}
 }
 
 
