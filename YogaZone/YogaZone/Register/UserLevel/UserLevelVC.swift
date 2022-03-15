21a26a386a358b01c0210e8564fa43e70b96785d
marcusungarette <marcusungarette@gmail.com>
2022-03-14 23:09:15 -0300

    Tela cadastro MainRegister feita

---
+++ YogaZone/YogaZone/Register/UserLevel/UserLevelVC.swift
@@ -0,0 +1,19 @@
+//
+//  UserLevelVC.swift
+//  YogaZone
+//
+//  Created by Marcus on 14/03/22.
+//
+
+import UIKit
+
+class UserLevelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Hide Back Button from UINavigationItem
+                self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+}
+    
+    
