4182dc04426647089e855616aba41aea8399efd3
marcusungarette <marcusungarette@gmail.com>
2022-03-16 23:56:16 -0300

    Tela cadastro UserLevel,MoreInfoRegister e DoneRegister feitas

---
+++ YogaZone/YogaZone/Register/MoreInfoRegister/RadioButtonController.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  YogaZone
+//
+//  Created by Marcus on 16/03/22.
+//
+
+import UIKit
+
+class RadioButtonController: NSObject {
+    var buttonsArray: [UIButton]! {
+        didSet {
+            for b in buttonsArray {
+                b.setImage(UIImage(named: "radio_off"), for: .normal)
+                b.setImage(UIImage(named: "radio_on"), for: .selected)
+            }
+        }
+    }
+    var selectedButton: UIButton?
+    var defaultButton: UIButton = UIButton() {
+        didSet {
+            buttonArrayUpdated(buttonSelected: self.defaultButton)
+        }
+    }
+
+    func buttonArrayUpdated(buttonSelected: UIButton) {
+        for b in buttonsArray {
+            if b == buttonSelected {
+                selectedButton = b
+                b.isSelected = true
+            } else {
+                b.isSelected = false
+            }
+        }
+    }
+}
--- YogaZone/YogaZone/Register/UserLevel/UserLevelVC.swift
+++ YogaZone/YogaZone/Register/UserLevel/UserLevelVC.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class UserLevelVC: UIViewController {
-
+    
+    @IBOutlet weak var tappedArrowVoltarImage: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Hide Back Button from UINavigationItem
-                self.navigationItem.setHidesBackButton(true, animated: true)
+        configImages()
+        configBackButton()
+        
     }
+    
+    
+    func configImages(){
+            tappedArrowVoltarImage.image = UIImage(named: "arrowVoltarCinza")
+        
+            // Hide Back Button from UINavigationItem
+                self.navigationItem.setHidesBackButton(true, animated: true)
+        }
+    
+    private func configBackButton(){
+        let tapBackButton = UITapGestureRecognizer(target: self, action: #selector(self.tappedBackButton))
+        self.tappedArrowVoltarImage.addGestureRecognizer(tapBackButton)
+        self.tappedArrowVoltarImage.isUserInteractionEnabled = true
+        
+    }
+    
+    @objc func tappedBackButton(){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func tappedLevelButton(){
+        self.navigationController?.pushViewController(MoreInfoRegisterVC(), animated: true)
+    }
+    
+    @IBAction func tappedBeginnerButton(_ sender: UIButton) {
+        self.tappedLevelButton()
+    }
+    
+    @IBAction func tappedLowExperienceButton(_ sender: UIButton) {
+        self.tappedLevelButton()
+    }
+    
+    @IBAction func tappedMediumExperienceButton(_ sender: UIButton) {
+        self.tappedLevelButton()
+    }
+    
+    @IBAction func tappedGuruButton(_ sender: UIButton) {
+        self.tappedLevelButton()
+    }
+    
 }
     
     
