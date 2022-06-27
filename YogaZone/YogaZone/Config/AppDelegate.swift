26c94402d6e7821b0c3bb657b61ca77c347057d6
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-06-26 21:33:44 -0300

    Including Google Places pods and excluding arm64 architecture from build settings

--- YogaZone/YogaZone/Config/AppDelegate.swift
+++ YogaZone/YogaZone/Config/AppDelegate.swift
@@ -1,7 +1,7 @@
 import UIKit
 import FirebaseCore
 import Firebase
-
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,7 +9,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         FirebaseApp.configure()
+        GMSPlacesClient.provideAPIKey(self.apiKey)
         return true
     }
+    
+}
 
+//MARK: - Get Api Keys from Keys.plist
+extension AppDelegate {
+    private var apiKey: String {
+           get {
+               guard let filePath = Bundle.main.path(forResource: "GooglePlacesKeys", ofType: "plist") else {
+                   fatalError("Couldn't find file 'GooglePlacesKeys.plist'.")
+               }
+               let plist = NSDictionary(contentsOfFile: filePath)
+               guard let value = plist?.object(forKey: "GooglePlacesKey") as? String else {
+                   fatalError("Couldn't find key 'GooglePlacesKey' in 'Keys.plist'.")
+               }
+               if (value.starts(with: "_")) {
+                   print("Register for a Google Cloud Platform developer account and get an API key at https://developers.google.com/maps/documentation/places/ios-sdk/cloud-setup")
+               }
+               return value
+           }
+       }
 }
